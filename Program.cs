6d347b27b1a5ca701145ebd156dc929fce3258a1
TomaSilva1 <tomasilva302@gmail.com>
2023-06-29 12:24:54 -0300

    1er commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// enum.GetName();
+//String texto = File.ReadAllText(RutaArchivo);
+//Console.WriteLine("Contenido :{0}" +texto); Muestra todo
+// List<string> LineasDelArchivo = File.ReadAllLines(RutaArchivo).ToList(); Lo transforma en una lista y lo puedo mostrar con foreach.
+//String[] MisLineas = {"Prueba1", "Prueba2"};
+//File.WriteAllLines(RutaArchivo, MisLineas); Pisa los archivos escritos anteriormente.
+
+//https://json2csharp.com/ → para convertir un archivo json a c#
+//http://jsonviewer.stack.hu/ → para visualizar el contenido de un archivo json
+
+
+namespace tp9
+{
+    class Program
+    {
+        static void main()
+        {
+            Console.WriteLine("Hello, World!");
+
+            
+        }
+    }
+}
